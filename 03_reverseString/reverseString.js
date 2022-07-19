761665d533e1f71e311e4e604ea61f5e49ef2fb7
Tyler <tyler.matsuzaki65@gmail.com>
2022-07-19 16:56:35 -0700

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string != ''){
+        var splitString = string.split("");
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join("");
+        return joinArray;
+    }else{
+        return "";
+    }
 };
 
 // Do not edit below this line
