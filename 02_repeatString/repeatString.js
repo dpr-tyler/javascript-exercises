802a774aeee1983183b6772aa5d285c1062c5d3d
Tyler <tyler.matsuzaki65@gmail.com>
2022-07-19 16:40:05 -0700

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var res = "";
+    if (num > 0){
+        for (i = 0; i < num; i++){
+            res += string;
+        }
+        return res;
+    }else if (num == 0){
+        return "";
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
